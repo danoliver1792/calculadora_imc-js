4eb1f6f0f5f879e526340cd61e6e2edb04079ca6
danrlei-oliveira <danrlei.jesus@hotmail.com>
2023-10-26 21:07:34 -0300

    ajustando o script para calcular o IMC

--- script.js
+++ script.js
@@ -2,13 +2,17 @@ const calcular = document.getElementById("calcular");
 
 function imc() {
     const nome = document.getElementById("nome").value;
-    const altura = document.getElementById("altura").value;
-    const peso = document.getElementById("peso").value;
+    const altura = parseFloat(document.getElementById("altura").value.replace(",", "."));
+    const peso = parseFloat(document.getElementById("peso").value.replace(",", "."));
     const resultado = document.getElementById("resultado");
 
     // verificando se os campos estao preenchidos
-    if (nome.value !== "" && altura !== "" && peso !== "") {
-        alert("preenchido");
+    if (nome !== "" && !isNaN(altura) && altura > 0 && !isNaN(peso)) {
+
+        const valorIMC = (peso / (altura * altura)).toFixed(2);
+
+        resultado.textContent = `IMC: ${valorIMC}`;
+
     } else {
         resultado.textContent = "Preencha todos os campos";
     }
