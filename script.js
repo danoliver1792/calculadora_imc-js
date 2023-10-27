f398eec46b3ea49b4fb45fe5d81d1520238aa562
danrlei-oliveira <danrlei.jesus@hotmail.com>
2023-10-26 21:21:03 -0300

    condicao para classificar o IMC

--- script.js
+++ script.js
@@ -11,7 +11,24 @@ function imc() {
 
         const valorIMC = (peso / (altura * altura)).toFixed(2);
 
-        resultado.textContent = `IMC: ${valorIMC}`;
+        // classificacao do IMC
+        let classificacao = "";
+
+        if (valorIMC < 18.5) {
+            classificacao = "abaixo do peso";
+        } else if (valorIMC < 25) {
+            classificacao = "com peso ideal";
+        } else if (valorIMC < 30) {
+            classificacao = "um pouco acima do peso";
+        } else if (valorIMC < 35) {
+            classificacao = "com obesidade grau 1";
+        } else if (valorIMC < 40) {
+            classificacao = "com obesidade grau 2";
+        } else {
+            classificacao = "com obesidade grau 3";
+        }
+
+        resultado.textContent = `${nome} seu IMC é ${valorIMC}, você está ${classificacao}`;
 
     } else {
         resultado.textContent = "Preencha todos os campos";
